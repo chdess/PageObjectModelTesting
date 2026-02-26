47c07924f6608cfecf9895a5d4a4194513a1b7b7
hahahades1 <164928406+hahahades1@users.noreply.github.com>
2026-02-26 19:11:58 +0500

    some little refactoring for readability

--- test_main_page.py
+++ test_main_page.py
@@ -3,6 +3,7 @@ from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
 import pytest
 
+
 @pytest.mark.login_guest
 class TestLoginFromMainPage():
     def test_guest_should_see_login_link(self, browser):
